853c972784d09bceadaa21e2c0fb9fdb20f55119
dmota6693@gmail.com <e107ti@etec.sp.gov.br>
2023-08-14 15:18:10 -0300

    adicionar e fechar

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("No céu azul");
+Console.WriteLine("Pássaro voa livre");
+Console.WriteLine("E me deixa,aqui sonhar");
+Console.WriteLine("");
+Console.WriteLine("Sandro da Silva Costa");
